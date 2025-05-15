dedd1ade9cddbc307eab424c781207b1a50f27dd
Pago-99 <dpcks9427@gmail.com>
2025-05-15 11:10:02 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="crypto_fft_ml_analysis",
+    version="0.1",
+    packages=find_packages(),  # this will include data_ingestion, preprocessing, etc.
+)
